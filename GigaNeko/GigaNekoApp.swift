30d631699734a047ee3a398565900179fdb34b08
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-10-17 16:22:09 +0900

    first commit

---
+++ GigaNeko/GigaNekoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigaNekoApp.swift
+//  GigaNeko
+//
+//  Created by 水原　樹 on 2024/10/17.
+//
+
+import SwiftUI
+
+@main
+struct GigaNekoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
