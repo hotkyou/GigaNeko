4fe74af11c1264b2960cf14c928819db416d5c57
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-10-18 16:08:34 +0900

    Firebaseの環境とMainTabViewの追加 AppをContentViewからMainTabViewに遷移

---
+++ GigaNeko/Feature/Tab/MainTabView.swift
@@ -0,0 +1,42 @@
+//
+//  TabView.swift
+//  GigaNeko
+//
+//  Created by 水原　樹 on 2024/10/18.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    @State private var selectedItem = 0
+    
+    var body: some View {
+        TabView(selection:$selectedItem){
+            HomeView()
+                .tabItem{
+                    Image(systemName: "house")
+                    Text("Home")
+                } .tag(0)
+            StatisticsView()
+                .tabItem {
+                    Image(systemName: "book.fill")
+                    Text("Word")
+                }.tag(1)
+            ShopView()
+                .tabItem {
+                    Image(systemName: "calendar")
+                    Text("Event")
+                }.tag(2)
+            SettingView()
+                .tabItem {
+                    Image(systemName: "gearshape")
+                    Text("setting")
+                }.tag(4)
+        }
+    }
+}
+
+
+#Preview {
+    MainTabView()
+}
