2470730516cf037e1e7d75a574d9ebf3a2606f16
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-10-18 14:04:56 +0900

    ファイル追加

---
+++ GigaNeko/Entites/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  GigaNeko
+//
+//  Created by 水原　樹 on 2024/10/18.
+//
+
+import Foundation
+import SwiftData
---
+++ GigaNeko/Feature/Statistics/StatisticsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatisticsView.swift
+//  GigaNeko
+//
+//  Created by 水原　樹 on 2024/10/18.
+//
+
+import SwiftUI
+
+struct StatisticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatisticsView()
+}
