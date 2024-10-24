a08bc122d2ae4e111f281aff3604cb76e4d0fb80
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-10-24 15:43:02 +0900

    家具のフォルダを追加

---
+++ GigaNeko/Feature/Furniture/FurnitureView.swift
@@ -0,0 +1,18 @@
+//
+//  FurnitureView.swift
+//  GigaNeko
+//
+//  Created by 水原　樹 on 2024/10/24.
+//
+
+import SwiftUI
+
+struct FurnitureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FurnitureView()
+}
