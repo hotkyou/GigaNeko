5b550ee734987f7f36828c7086b7dfa47ec0d1ca
hotkyou <hottarakasi.stu@gmail.com>
2024-10-25 16:45:09 +0900

    update(home): UI名前基盤追加と仮アイコン追加

--- GigaNeko/Feature/Home/HomeView.swift
+++ GigaNeko/Feature/Home/HomeView.swift
@@ -11,15 +11,14 @@ struct HomeView: View {
                     .offset(x: 0, y: 0) // オフセットを指定し、上部分を表示
             }
             HStack {
-                let screenWidth = geometry.size.width
                 let leftPadding = 30.0 // 左側の隙間を定義
-
+                
                 // ギガ表示のZStack
                 ZStack {
                     Rectangle()
                         .fill(Color.white)
                         .frame(width: 80, height: 80)
-                        .opacity(0.5)
+                        .opacity(0.7)
                         .cornerRadius(20)
                     
                     // メインのテキスト
@@ -53,13 +52,44 @@ struct HomeView: View {
                             Rectangle()
                                 .fill(Color.white)
                                 .frame(width: 230, height: 34)
-                                .opacity(0.5)
+                                .opacity(0.7)
                                 .cornerRadius(20)
                             
-                            // Rectangleの中のテキスト
-                            Text("餌の残り時間")
-                                .foregroundColor(.gray)
-                                .font(.system(size: 14))
+                            HStack {
+                                Spacer()
+                                Image("Stamina")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                    .offset(x: 0, y: 0) // オフセットを指定し、上部分を表示
+                                VStack(alignment: .leading) {
+                                    
+                                    // Rectangleの中のテキスト
+                                    Text("あと 12:40")
+                                        .foregroundColor(.gray)
+                                        .font(.system(size: 9))
+                                    
+                                    ProgressView(value: 0.5)
+                                        .scaleEffect(x: 1, y: 2)
+                                }
+                                Spacer()
+                                Divider()
+                                Spacer()
+                                Image("Stress")
+                                    .resizable()
+                                    .frame(width: 20, height: 20)
+                                VStack(alignment: .leading) {
+                                    
+                                    // Rectangleの中のテキスト
+                                    Text("ストレス値")
+                                        .foregroundColor(.gray)
+                                        .font(.system(size: 9))
+                                    
+                                    ProgressView(value: 0.5)
+                                        .scaleEffect(x: 1, y: 2)
+                                }
+                                Spacer()
+                            }
+                            .frame(width: 230, height: 34)
                         }
                         
                         HStack {
@@ -68,22 +98,60 @@ struct HomeView: View {
                                 Rectangle()
                                     .fill(Color.white)
                                     .frame(width: 80, height: 23) // 元の幅を維持
-                                    .opacity(0.5)
+                                    .opacity(0.7)
                                     .cornerRadius(20)
                                 
-                                // Rectangleの中のテキスト
-                                Text("3000pt")
-                                    .foregroundColor(.gray)
-                                    .font(.system(size: 12))
+                                HStack {
+                                    Image("Point")
+                                        .resizable()
+                                        .frame(width: 20, height: 20)
+                                    // Rectangleの中のテキスト
+                                    Text("3000pt")
+                                        .foregroundColor(.gray)
+                                        .font(.system(size: 12))
+                                }
                             }
                         }
                         .frame(width: 230) // Text1と同じ幅のコンテナを作成
                     }
-                }
+                } // HStack
                 .padding(.trailing, leftPadding)
                 .padding(.top, 65)
+            } // HStack
+            
+            Spacer()
+            
+            VStack {
+                Spacer()
+                    .frame(height: UIScreen.main.bounds.height * 0.85) // 画面の高さの9/10
+                HStack {
+                    Spacer()
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.white)
+                            .opacity(0.7)
+                            .cornerRadius(20)
+                        HStack {
+                            Text("Lv100")
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.gray)
+                                .font(.system(size: 12))
+                            Divider()
+                                .frame(height: 15)
+                            Text("nekopoyo")
+                                .multilineTextAlignment(.center)
+                                .foregroundColor(.gray)
+                                .font(.system(size: 12))
+                            
+                        }
+                    }
+                    .frame(width: 150, height: 25)
+                    Spacer()
+                }
+                Spacer()
             }
-        }
+            
+        } // GeometryReader
         .edgesIgnoringSafeArea(.all) // 全画面表示
     }
 }
