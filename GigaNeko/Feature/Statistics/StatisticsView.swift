c50b0306d9a22ad05106b9cfacac6c54cdb73e09
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-11-01 22:50:33 +0900

    背景追加、モバイルデータ画面の取得中

--- GigaNeko/Feature/Statistics/StatisticsView.swift
+++ GigaNeko/Feature/Statistics/StatisticsView.swift
@@ -8,9 +8,49 @@
 import SwiftUI
 
 struct StatisticsView: View {
+    @State private var selectedSegment = "月"
+    
     var body: some View {
-        Text(saveData())
+        ZStack{
+            //カラーストップのグラデーション
+            Image("StaticBackGround")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HStack(spacing: 20) {
+                    SegmentButton(title: "日", isSelected: selectedSegment == "日") {
+                        selectedSegment = "日"
+                    }
+                    SegmentButton(title: "週", isSelected: selectedSegment == "週") {
+                        selectedSegment = "週"
+                    }
+                    SegmentButton(title: "月", isSelected: selectedSegment == "月") {
+                        selectedSegment = "月"
+                    }
+                }
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(20)
+                
+            }
+        }
         
+    }
+}
+
+struct SegmentButton: View {
+    let title: String
+    let isSelected: Bool
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .foregroundColor(isSelected ? .white : .gray)
+                .padding()
+                .background(isSelected ? Color.orange : Color.clear)
+                .cornerRadius(20)
+        }
     }
 }
 
