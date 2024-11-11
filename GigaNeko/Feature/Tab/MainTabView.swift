5526f98facdf8850f756723668c084ffdababaec
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-11-11 14:41:42 +0900

    MainTabをの変更

--- GigaNeko/Feature/Tab/MainTabView.swift
+++ GigaNeko/Feature/Tab/MainTabView.swift
@@ -17,7 +17,7 @@ struct MainTabView: View {
                     Image(systemName: "house")
                     Text("Home")
                 } .tag(0)
-            MobileDataGetView()
+            FurnitureView()
                 .tabItem {
                     Image(systemName: "book.fill")
                     Text("Word")
