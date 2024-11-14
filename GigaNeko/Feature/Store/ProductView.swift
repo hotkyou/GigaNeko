8b2677a314fc126561b5e5515a3a8ebe3e631a83
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-11-14 16:23:57 +0900

    ShopViewに選択された時のアニメーションを追加

--- GigaNeko/Feature/Store/ProductView.swift
+++ GigaNeko/Feature/Store/ProductView.swift
@@ -7,41 +7,48 @@
 
 import SwiftUI
 
+struct ProductGrid: View {
+    let products: [(String, Int)]
+    let columns: [GridItem]
+    
+    var body: some View {
+        LazyVGrid(columns: columns, spacing: 20) {
+            ForEach(products, id: \.0) { product in
+                ProductView(productName: product.0, productPrice: product.1)
+            }
+        }
+    }
+}
+
 struct ProductView: View {
     let productName: String
     let productPrice: Int
-
+    
     var body: some View {
         VStack(spacing: 15) {
-            // 上部の四角いボックス
             RoundedRectangle(cornerRadius: 10)
                 .fill(Color.gray.opacity(0.3))
                 .frame(width: 100, height: 100)
             
-            // ラベル
             Text(productName)
                 .font(.body)
-              
-            // ステッパー
-            HStack{
+            
+            HStack {
                 RoundedRectangle(cornerRadius: 8)
-                    .fill(Color.gray.opacity(0.4)) // グレーの背景
-                    .frame(width: 25, height: 25) // サイズ調整
-                    .padding(.leading, 5) // 左側に少し余白
+                    .fill(Color.gray.opacity(0.4))
+                    .frame(width: 25, height: 25)
+                    .padding(.leading, 5)
                 
                 Text("\(productPrice)")
-                    .font(.system(size: 20, weight: .medium)) // フォントサイズとスタイル
-                    .foregroundColor(.black) // 文字色
-                    .padding(.horizontal,5) // 左側に少し余白
+                    .font(.system(size: 20, weight: .medium))
+                    .foregroundColor(.black)
+                    .padding(.horizontal, 5)
             }
-            .frame(height: 40) // 全体の高さを設定
-            .background(Color(red: 0.95, green: 0.95, blue: 0.95)) // 外側の背景色を調整
-            .cornerRadius(5) // 全体の角丸
+            .frame(height: 40)
+            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+            .cornerRadius(5)
             
-        }.padding()
+        }
+        .padding()
     }
-}
-
-#Preview {
-    ProductView(productName: "普通のエサ", productPrice: 500)
 }
